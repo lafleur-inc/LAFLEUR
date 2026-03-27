ebd0c6f713103f6b9aa98d48a018987c7325be8d
1jason <jasonnotin@gmail.com>
2026-03-27 10:22:10 +0100

    burger

--- accueilLaFleur.php
+++ accueilLaFleur.php
@@ -10,16 +10,14 @@
         </head>
         <body>
             <h1>Recherche dans notre catalogue de fleurs</h1>
-            <legend >Choisissez un type de fleur</legend>
-                <form action="recherche.php" method="post">
-                    <select class="deroulant" id="typeRecherche" name="typeRecherche" required><br>
-                        <option value="title">Titre</option>
-                        <option value="author">Auteur</option>
-                        <option value="isbn">ISBN</option>
-                    </select> <br><br>
-                    <label for="termeRecherche">Terme a rechercher :</label> <br>
-                        <input type="text" name="termeRecherche">
-                        <input type="submit" value="Rechercher">
-            </legend>
+            <nav>
+                <label for="toggle">☰</label>
+                <input type="checkbox" id="toggle">
+                <div class="main_pages">
+                    <a href="#">Accueil</a>
+                    <a href="#">Actualité</a>
+                    <a href="#">Contact</a>
+                </div>
+            </nav>
         </body>
     </html>
