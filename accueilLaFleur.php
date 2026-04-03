bcba128ca3195b0afcf9035e10a7fd42c2950d97
leplot67 <lbrou421@gmail.com>
2026-04-03 09:50:10 +0200

    rajout image connexion

--- accueilLaFleur.php
+++ accueilLaFleur.php
@@ -6,11 +6,17 @@
         <head>
             <meta charset="UTF-8">
             <title>Page d'accueil La Fleur</title>
-            <link rel="stylesheet" href="style.css">
+            <link rel="stylesheet" href="style Leo.css">
         </head>
         <header>
             <h1>Recherche dans notre catalogue de fleurs</h1>
             <a href="login.php">connexion</a>
+            <a href="login.php">
+            <img src="https://static.vecteezy.com/ti/vecteur-libre/p1/26626361-compte-icone-vecteur-symbole-conception-illustration-vectoriel.jpg" 
+                alt="silhouette de connexion"
+                width="50"
+                height="50"
+            />
         </header>
         <body>
             <nav>
