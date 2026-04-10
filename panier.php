1860c733e143b925632e6c95f12d5f1296427eb9
Krokmakeu <louis.schmid45@gmail.com>
2026-04-10 08:47:00 +0200

    Ajout Css panier

--- panier.php
+++ panier.php
@@ -8,6 +8,8 @@ include_once("fonctionPanier.php");
 <html xmlns="http://www.w3.org/1999/xhtml" xml : lang="fr">
 <head>
 
+<link rel="stylesheet" href="style.css">
+
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Panier</title>
 
